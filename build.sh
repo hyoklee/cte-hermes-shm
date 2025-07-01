b3d0547d2c11d83b944d60d6bb9a75f68adfebcc
H. Joe Lee <hyoklee@hdfgroup.org>
2025-07-01 18:52:30 -0500

    build(conda): do not run env.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 export CMAKE_PREFIX_PATH=$PREFIX
 export CMAKE_INCLUDE_PATH=$PREFIX/include
 export PKG_CONFIG_PATH=$CONDA_PREFIX/lib/pkgconfig:$PKG_CONFIG_PATH
-sh env.sh
 cmake -B build .
 make -C build -j 4
 make -C build install
