a6f8d6ca7518480dadee6506d17ce5731fa0f04e
H. Joe Lee <hyoklee@hdfgroup.org>
2025-07-01 20:57:46 -0500

    build(conda): set prefix

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 export CMAKE_PREFIX_PATH=$PREFIX
 export CMAKE_INCLUDE_PATH=$PREFIX/include
 export PKG_CONFIG_PATH=$CONDA_PREFIX/lib/pkgconfig:$PKG_CONFIG_PATH
-cmake -B build .
-make -C build -j 4
+cmake -B build -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX .
+make -C build -j 2
 make -C build install
